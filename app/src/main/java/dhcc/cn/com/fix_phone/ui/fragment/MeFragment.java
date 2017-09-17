e33934b7ebccbc03113129bd15a610246c2f3c76
PC-20150618IZBT\Administrator <songyang4586@gmail.com>
2017-09-17 21:21:02 +0800

    增加我的模块UI

--- app/src/main/java/dhcc/cn/com/fix_phone/ui/fragment/MeFragment.java
+++ app/src/main/java/dhcc/cn/com/fix_phone/ui/fragment/MeFragment.java
@@ -1,7 +1,10 @@
 package dhcc.cn.com.fix_phone.ui.fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import dhcc.cn.com.fix_phone.R;
 import dhcc.cn.com.fix_phone.base.BaseFragment;
 
@@ -21,5 +24,23 @@ public class MeFragment extends BaseFragment {
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_me;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+    }
+
+    @OnClick({R.id.mine_info, R.id.mine_circle, R.id.mine_house, R.id.mine_suggest, R.id.mine_vip,
+            R.id.mine_advert, R.id.mine_produce, R.id.mine_clear, R.id.mine_app})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.mine_circle:
+
+                break;
+            default:
+                break;
+        }
     }
 }
