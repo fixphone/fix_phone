7f33175cb39c5f591b33c07601eb90c86e2d0ec9
PC-20150618IZBT\Administrator <songyang4586@gmail.com>
2017-10-09 23:25:40 +0800

    有话用户信息

---
+++ app/src/main/java/dhcc/cn/com/fix_phone/bean/LoginInfo.java
@@ -0,0 +1,8 @@
+package dhcc.cn.com.fix_phone.bean;
+
+/**
+ * Created by Administrator on 2017/10/9 0009.
+ */
+
+public class LoginInfo {
+}
---
+++ app/src/main/java/dhcc/cn/com/fix_phone/ui/activity/ConversationActivity.java
@@ -0,0 +1,8 @@
+package dhcc.cn.com.fix_phone.ui.activity;
+
+/**
+ * Created by Administrator on 2017/10/9 0009.
+ */
+
+public class ConversationActivity {
+}
